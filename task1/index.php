437008d41ae335e06ce1f1e2eb5322f9793b7c5e
d-sirotkin <sirotkina02062005@gmail.com>
2025-11-28 17:00:20 +0300

    Упражнение 1

---
+++ task1/index.php
@@ -0,0 +1,11 @@
+
+
+<form method = "get" action = "age_check.php">
+    <input  type = "text" id = "name" name = "name" placeholder = "Введите имя.">
+    <input  type = "number" id = "age" name = "age" placeholder = "Введите возраст.">
+    <input type = "submit" value = "Отправить">
+</form>
+
+<?php 
+// выше расположена сама форма с методом GET и атрибутом action
+// который указывает куда будут отправлены данные
